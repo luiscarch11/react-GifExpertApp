df26cdf3a516a631c47fef8bdbfa0c89589977e6
Luis Utrera <luis.carch1998@gmail.com>
2020-08-16 12:15:11 -0400

    docs added

---
+++ docs/precache-manifest.33a23924c7c7e470754b4365d949261d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d96f88343184a517f7978a5c3a1b09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "464daa08c19ad0237a7f",
+    "url": "/static/css/main.b7d0c588.chunk.css"
+  },
+  {
+    "revision": "bfcd43a512caafc97689",
+    "url": "/static/js/2.09d96370.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09d96370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464daa08c19ad0237a7f",
+    "url": "/static/js/main.eae564d1.chunk.js"
+  },
+  {
+    "revision": "2adeb7d60292ec46b5e8",
+    "url": "/static/js/runtime-main.beb90681.js"
+  }
+]);
